fb5bfb6ea82606be9c5264904387529f907b91c4
max <mfilimonov@smartru.com>
2019-07-11 12:27:53 +0300

    prrr

---
+++ test.rb
@@ -0,0 +1,15 @@
+There’s a goblin at my window,
+
+A monster by my door.
+
+The pumpkin at my table
+
+Keeps on smiling more and more.
+
+There’s a ghost who haunts my bedroom,
+
+A witch whose face is green.
+
+They used to be my family,
+
+Till they dressed for Halloween.
